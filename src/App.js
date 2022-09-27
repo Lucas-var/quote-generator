387f2b25656319128968ea01af3c2ec2c8be7595
Lucas <lucas.beraldin@gmail.com>
2022-09-27 17:26:17 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Quote from './Component/Quote';
+
+function App() {
+
+  return (
+
+    <div className="App">
+      <Quote />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Quote.js
@@ -0,0 +1,21 @@
+
+
+const Quote = () => {
+
+    return (
+        <div className="quote">
+
+            <div className="anime" title="Naruto"> Naruto </div>
+
+            <blockquote>
+                There is a powerful driving force inside every human being that, once unleashed, can make any vision, dream or desire a reality
+            </blockquote>
+
+            <div className="character" title="Madara"> Maadara </div>
+
+        </div>
+
+    );
+}
+
+export default Quote;
